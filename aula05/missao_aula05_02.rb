067ffeb401d4a0c04cebf5df54f5d8af0e79ab0d
Heverton S Costa <hevertonscosta@gmail.com>
2021-01-24 13:10:47 -0300

    Update files relatives the lesson 05

--- aula05/missao_aula05_02.rb
+++ aula05/missao_aula05_02.rb
@@ -1,18 +1,38 @@
 require 'cpf_cnpj'
 
+# def verifica(cpf)
+#     return CPF.valid?(cpf)
+
+# end
+
+# puts ''
+# puts 'Digite o CPF a ser verificada a validade : '
+# cpf = gets.chomp.to_i
+# puts ''
+
+# if verifica(cpf)
+#     puts 'CPF digitado é VÁLIDO! '
+# else
+#     puts 'CPF digitado é INVÁLIDO! '
+# end
+# puts ''
+
+# redigitando de acordo com a correção que vi no topico posterir da aula
+
 def verifica(cpf)
-    return CPF.valid?(cpf)
+    if CPF.valid?(cpf)
+        return 'O CPF informado é VÁLIDO!'
+    else
+        return 'O CPF informado é INVÁLIDO!'
+    end
+
+    # return CPF.valid?(cpf)
 
 end
+
 
 puts ''
 puts 'Digite o CPF a ser verificada a validade : '
 cpf = gets.chomp.to_i
-puts ''
-
-if verifica(cpf)
-    puts 'CPF digitado é VÁLIDO! '
-else
-    puts 'CPF digitado é INVÁLIDO! '
-end
+puts verifica(cpf)
 puts ''
